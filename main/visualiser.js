02802503f56b762a33df274d8ab365e27b419c35
hazza <harrystenholm@yahoo.com>
2026-03-06 19:00:31 +0000

    Added new option for plot type display with waveform and frequency distribution.

--- main/visualiser.js
+++ main/visualiser.js
@@ -6,7 +6,8 @@ const overlay = document.getElementById('overlay');
 const settingsToggle = document.getElementById('advanced');
 const sidePanel = document.getElementById('side-panel');
 
-const themeInput = document.getElementById('colorTheme');
+const typeInput = document.getElementById('type');
+const themeInput = document.getElementById('theme');
 const modeInput = document.getElementById('mode');
 
 const trailCountInput = document.getElementById('trailCount');
@@ -50,6 +51,8 @@ function draw() {
     requestAnimationFrame(draw);
     frameCount++;
 
+    const type = typeInput.value;
+
     // retrieves num trails, glow, mode and theme input values
     const numTrails = parseInt(trailCountInput.value);
     const theme = themeInput.value;
@@ -72,14 +75,9 @@ function draw() {
     ctx.save();
     ctx.translate(centerX, centerY);
     ctx.scale(scale, scale);
-
-
-    //initialise volume and ellipse target size
-    let bassAvg = data[0] / 255; 
-    let targetSize = (bassAvg * 150);
-    currentSize += (targetSize - currentSize) * 0.35; // Lerp
-
-    // initiliase colour changing based on volume
+    
+    let bassAvg = data[0] / 255 || 0; 
+    // initiliase colour theme based on volume
     // (0 is red, 120 is green, 240 is blue)
     if (theme === 'ocean') {
         hue = 200 + (bassAvg * 40);
@@ -91,13 +89,27 @@ function draw() {
         hue = 100 + (bassAvg * 40);
         lightness = 15;
     }
+    for (let i = 0; i < numSpikes; i++) {
+
+    }
+
+    if (type === 'default') drawDefault(bassAvg, hue, lightness, numTrails, mode, data);
+    else if (type === 'waveform') drawWaveform(hue, lightness, mode, data);
+    
+    ctx.restore();
+}
+
+function drawDefault(bassAvg, hue, lightness, numTrails, mode, data) {
+    if (!data) return;
+    //initialise volume and ellipse target size
+    let targetSize = (bassAvg * 150);
+    currentSize += (targetSize - currentSize) * 0.35; // Lerp
     
     // initialise outline
     let currentFramePoints = [];
-    ctx.beginPath();
+    ctx.beginPath();    
     for (let i = 0; i < numSpikes; i++) {
         let rawAmp = 0;
-
         if (mode === 'waveform') {
             let waveId = Math.floor(i * (data.length / numSpikes));
             let amplitude = Math.abs(data[waveId] - 128) / 128;
@@ -105,7 +117,6 @@ function draw() {
         } else if (mode === 'frequency') {
             rawAmp = (data[i * 2] / 255) * 150;
         }
-
         // Gravity logic
         if (rawAmp > spikeHeights[i]) spikeHeights[i] = rawAmp;
         else spikeHeights[i] = Math.max(0, spikeHeights[i] - gravity);
@@ -115,13 +126,14 @@ function draw() {
         let x = Math.cos(angle) * r;
         let y = Math.sin(angle) * r;
         
-        let style = `hsla(${hue}, 80%, ${lightness}, 0.8)`;
-        ctx.shadowBlur = 15 + (i * 5); 
+        let style = `hsla(${hue}, 80%, ${lightness}%, 0.8)`;
+        
+        ctx.shadowBlur = 15; 
         ctx.shadowColor = style;
-
-        // 3. Spikes with Gravity
         ctx.strokeStyle = style;
-        ctx.lineWidth = 3;
+        ctx.lineWidth = 1;
+
+        // connect the lines
         if (i === 0) ctx.moveTo(x, y);
         else ctx.lineTo(x, y);
 
@@ -143,7 +155,7 @@ function draw() {
     // for each trail item, calculate points and draw 
     trailItems.forEach((points, index) => {
         //create trail offset to scale each trail
-        let offset = 1 + (index * 0.01);
+        let offset = 1 + (index * 0.05);
 
         let opacity = (index + 1) / (trailItems.length + 1);
         ctx.strokeStyle = `hsla(${hue + (index * 5)}, 80%, ${lightness}%, ${opacity * 0.5})`;
@@ -161,7 +173,7 @@ function draw() {
     });
     
     const ellipseCount = parseInt(ellipseCountInput.value);
-    for (i = 0; i < ellipseCount; i++){
+    for (let i = 0; i < ellipseCount; i++){
         // initialise ellipse
         ctx.beginPath();
         let innerHue = hue + (i * 10);
@@ -184,8 +196,41 @@ function draw() {
 
         ctx.shadowBlur = 0;
     }
-    
-    ctx.restore();
+}
+
+function drawWaveform(hue, lightness, mode, data) {
+    if (!data) return;
+    for (let j = 0; j < 2; j++) {
+        ctx.beginPath();   
+        let style = `hsla(${hue}, 80%, ${lightness}%, 0.8)`;
+        ctx.strokeStyle = style;
+        ctx.lineWidth = 1;
+
+        for (let i = 0; i < numSpikes; i++) {
+            let rawAmp = 0;
+            if (mode === 'waveform') {
+                let waveId = Math.floor(i * (data.length / numSpikes));
+                let amplitude = Math.abs(data[waveId] - 128) / 128;
+                rawAmp = amplitude * 150;
+            } else if (mode === 'frequency') {
+                rawAmp = (data[i * 2] / 255) * 150;
+            }
+            let x, y;
+            let perc = i / numSpikes
+            if (j === 0) {
+                x = -300 + (600 * perc);
+                y = -rawAmp;
+            } else if (j === 1) {
+                x = 300 - (600 * perc);
+                y = rawAmp;
+            }
+
+            // connect the lines
+            if (i === 0) ctx.moveTo(x, y);
+            else ctx.lineTo(x, y)
+        }
+        ctx.stroke();
+    }
 }
 
 overlay.addEventListener('click', () => {
